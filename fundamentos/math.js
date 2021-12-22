c1f392b0056a8a8f461aba3aa26a675d56a8b465
IuryC <iurycauan0352@hotmail.com>
2021-12-22 13:30:12 -0300

    22/12/2021

---
+++ fundamentos/math.js
@@ -0,0 +1,4 @@
+const raio = 5.6;
+const areaDiametro = Math.PI * Math.pow(raio, 2);
+
+console.log(areaDiametro)
---
+++ fundamentos/templateString.js
@@ -0,0 +1,5 @@
+const nome = "Rebeca";
+const concatenacao = "Olá " + nome + "!";
+console.log(concatenacao)
+
+console.log(`1 + 1 = ${1 + 1}`);
