c8a06748433ea87447001e235e6857cb36da4ee3
IuryC <iurycauan0352@hotmail.com>
2021-12-22 13:29:57 -0300

    22/12/2021

---
+++ fundamentos/strings.js
@@ -0,0 +1,10 @@
+const escola = "coder";
+
+console.log(escola.charAt(4));
+console.log(typeof charAt);
+console.log(escola.charCodeAt(3));
+console.log(escola.substring(1));
+console.log(escola.substring(0, 3));
+console.log('Escola '.concat(escola).concat("!"))
+
+console.log("Ana, Maria, Pedro".split(","))
